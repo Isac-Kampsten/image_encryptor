1a08fbc09c346d858be0a902b602b41b5beaa043
Isac-Kampsten <isac.kampsten@elev.ga.ntig.se>
2023-11-21 10:44:24 +0100

    Lil update

--- main.py
+++ main.py
@@ -47,15 +47,43 @@ for y in range(height):
 # Loop through chosen pixels in sets of three and modify the RGB values
 binary_message = stringToBinary(message)
 message_index = 0
+new_pixels = []
 
-print(chosen_pixels)
+
 
 for i in range(0, len(chosen_pixels), 3):
     set_of_pixels = []
+
     for j in range(3):
         pixel = chosen_pixels[i + j]
         set_of_pixels.append(pixel)
+
     #get all rgb values for the set of pixels
+    r1, r2, r3 = set_of_pixels[0][0], set_of_pixels[1][0], set_of_pixels[2][0]
+    g1, g2, g3 = set_of_pixels[0][1], set_of_pixels[1][1], set_of_pixels[2][1]
+    b1, b2, b3 = set_of_pixels[0][2], set_of_pixels[1][2], set_of_pixels[2][2]
+
+    print(set_of_pixels)
+
+    rgb_values = [r1, g1, b1, r2, g2, b2, r3, g3, b3]
+
+    #modify every value accordingly except the last blue value, since it will be used to determine if the message continues or not
+    
+    for rgb_index in range(0,8):
+        if binary_message[message_index] == "0":
+            rgb_values[rgb_index] = rgb_values[rgb_index]
+            message_index += 1
+        else:
+            rgb_values[rgb_index] +- 1
+            message_index += 1
+
+        
+    
+    
+
+
+   
+
 
 
 
